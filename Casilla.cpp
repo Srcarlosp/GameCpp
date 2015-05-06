5d739761d85c77221c942287c5e3776fae407ce3
Plozano94 <plozano94@gmail.com>
2015-05-07 00:47:45 +0200

    Primera introduccion a la segmentacion de las fases
    Inicio de menu, tecla escape.

--- Casilla.cpp
+++ Casilla.cpp
@@ -1,4 +1,6 @@
 #include "Casilla.h"
+#include "glut.h"
+
 
 Casilla::Casilla()
 {
@@ -26,4 +28,62 @@ bool Casilla::getFull()
 Elemento * Casilla::getElem()
 {
 	return e;
+}
+void Casilla::pintarHexagono(float x2, float y2){
+	//angulo paralelo a z
+	int cont = 0;
+
+
+	while (cont <= 6){
+		float x = x2, y = y2;
+		if (cont == 1){
+			y = y2 + 0.866;
+		}
+		if (cont == 2){
+			x = x2 + 0.75;
+			y = y2 + 0.433;
+		}
+		if (cont == 3){
+			x = x2 + 0.75;
+			y = y2 - 0.433;
+		}
+		if (cont == 4){
+			y = y2 - 0.866;
+		}
+		if (cont == 5){
+			x = x2 - 0.75;
+			y = y2 - 0.433;
+		}
+		if (cont == 6){
+			x = x2 - 0.75;
+			y = y2 + 0.433;
+		}
+
+
+
+
+
+		glDisable(GL_LIGHTING);
+		glBegin(GL_POLYGON);
+
+
+		glColor3ub(100, 250, 250);
+		glVertex3f(x + 0.25, y + 0.433, 0.01);
+		glColor3ub(100, 250, 250);
+		glVertex3f(x + 0.5, y, 0.01);
+		glColor3ub(100, 250, 250);
+		glVertex3f(x + 0.25, y - 0.433, 0.01);
+		glColor3ub(100, 250, 250);
+		glVertex3f(x - 0.25, y - 0.433, 0.01);
+		glColor3ub(100, 250, 250);
+		glVertex3f(x - 0.5, y, 0.01);
+		glColor3ub(100, 250, 250);
+		glVertex3f(x - 0.25, y + 0.433, 0.01);
+		glEnd();
+		glEnable(GL_LIGHTING);
+		cont++;
+	}
+
+
+
 }
--- Elemento.cpp
+++ Elemento.cpp
@@ -1,10 +1,10 @@
 ﻿#include <cmath>
 #include "Elemento.h"
-#include "Interfaz.h"
 #include "glut.h"
+#include "Casilla.h"
 
 
-Interfaz interfaz1;   //Comentar que hace aqui????   Quedaria maejor pasarle un argumento a dodraw con la direccion de la interfaz o algo 
+Casilla casilla;   //Comentar que hace aqui????   Quedaria maejor pasarle un argumento a dodraw con la direccion de la interfaz o algo 
 
 Elemento::Elemento(int *p, int _x, int _y, unsigned char _r, unsigned char _g, unsigned char _b) :posAbs(_x, _y), ship(p)
 {
@@ -48,5 +48,5 @@ void Elemento::doDraw()
 	glTranslatef(this->posVec[y] * scale + offx, this->posVec[x] * scale + offy, this->posVec[z]);
 	glutSolidCube(0.3);
 	glTranslatef(-(this->posVec[y] * scale + offx), -(this->posVec[x] * scale + offy), -this->posVec[z]);
-	interfaz1.pintarHexagono(this->posVec[y] * scale + offx, this->posVec[x] * scale + offy);    //mmm...  No es bueno mezclar las cosas, lo mejor es que el elmento sepa pintarse a si mismo y que la casilla sepa tambien
+	casilla.pintarHexagono(this->posVec[y] * scale + offx, this->posVec[x] * scale + offy);    //mmm...  No es bueno mezclar las cosas, lo mejor es que el elmento sepa pintarse a si mismo y que la casilla sepa tambien
 }
